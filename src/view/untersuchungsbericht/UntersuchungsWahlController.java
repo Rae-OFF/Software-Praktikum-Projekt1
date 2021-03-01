f3cc5490fabf7663e0549697c32496dba0415a42
Mark <mark.ammermueller@tu-dortmund.de>
2021-03-01 22:44:24 +0100

    Untersuchungsbericht sortiert

--- src/view/untersuchungsbericht/UntersuchungsWahlController.java
+++ src/view/untersuchungsbericht/UntersuchungsWahlController.java
@@ -1,11 +1,11 @@
-package view.ueberweisung;
+package view.untersuchungsbericht;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TitledPane;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
-public class untersuchungsWahlController {
+public class UntersuchungsWahlController {
 
     @FXML
     private TitledPane Bericht1;
