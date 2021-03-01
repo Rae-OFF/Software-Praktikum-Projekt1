a7e2b7273120be1be42ee0cf04d660ad5a4c3ca8
Rui Yan <rui.yan@tu-dortmund.de>
2021-03-01 19:52:44 +0100

    Zusammenfassung von Behandelungsschritt

--- src/view/zusammenfassung der Behandlungsschritt/Zusammenfassung.kt
+++ src/view/zusammenfassung der Behandlungsschritt/Zusammenfassung.kt
@@ -6,10 +6,9 @@ package view.`zusammenfassung der Behandlungsschritt`
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.text.TextFlow;
-
 
 public class Zusammenfassung {
+
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -17,12 +16,8 @@ public class Zusammenfassung {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML // fx:id="pDaten"
-    private TextFlow pDaten; // Value injected by FXMLLoader
-
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert pDaten != null : "fx:id=\"pDaten\" was not injected: check your FXML file 'Seite von Zusammenfassung.fxml'.";
 
     }
 }
