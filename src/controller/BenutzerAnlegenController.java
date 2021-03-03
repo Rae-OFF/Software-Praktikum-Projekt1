10e2d5c598b78e0d6b6ca31ad41e910d58adf979
mohamed <sopr060>
2021-03-03 05:15:40 +0100

    BenutzerAnlegenController PatientDatenController

--- src/controller/BenutzerAnlegenController.java
+++ src/controller/BenutzerAnlegenController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import model.Arzt;
+import model.EPA;
+import model.Patient;
+
 import java.lang.UnsupportedOperationException;
 import java.util.List;
 
@@ -11,27 +15,51 @@ public class BenutzerAnlegenController {
     private EPAController ePAController;
 
     public BenutzerAnlegenController(EPAController e) {
+    	this.ePAController = e ;
     }
 
     /**
  	 *
  	 * TODO: create JavaDoc. 
- 	 * @param info
- 	 * @throws UnsupportedOperation Exception
+	 * @param Vorname Datum und Uhrzeit
+	 * @param Nachname des Patienten
+	 * @param fach -ikamente des Patienten
+	 * @param num des Patienten
+	 * @param passwort des Patienten
+	 * @param Tel des Patienten
+ 	 * @throws UnsupportedOperationException
  	 *	 	 	Diese Exception wird geworfen, fallsdie Methode noch nicht implementiert ist. 
  	 */
-    public void ArztAnlegen(List<String> info) throws UnsupportedOperationException {
+    public void ArztAnlegen(String Vorname,String Nachname, String fach, String num, String passwort, String Tel)
+			throws UnsupportedOperationException {
+
+             // neuen Artzt Anlegen
+			Arzt arzt = new Arzt(Vorname, Nachname, fach, num, passwort, Tel);
+
+
+
+
         throw new UnsupportedOperationException("Not Yet Implemented!");
     }
 
     /**
  	 *
  	 * TODO: create JavaDoc. 
- 	 * @param info
- 	 * @throws UnsupportedOperation Exception
+	 * @param versicherungsnummer des Patienten
+	 * @param Vorname Datum und Uhrzeit
+	 * @param Nachname des Patienten
+	 * @param Address -ikamente des Patienten
+	 * @param Geschlecht des Patienten
+	 * @param pass des Patienten
+ 	 * @throws UnsupportedOperationException
  	 *	 	 	Diese Exception wird geworfen, fallsdie Methode noch nicht implementiert ist. 
  	 */
-    public void PatientAnlegen(List<String> info) throws UnsupportedOperationException {
-        throw new UnsupportedOperationException("Not Yet Implemented!");
+    public void PatientAnlegen(String versicherungsnummer, String Vorname,String Nachname, String Address, String Geschlecht, String Birth, String pass)
+			throws UnsupportedOperationException {
+    	//neuen Patient anlegen
+		Patient patient = new Patient(versicherungsnummer, Vorname, Nachname, Address, Geschlecht, Birth, pass) ;
+
+
+		throw new UnsupportedOperationException("Not Yet Implemented!");
     }
 }
--- src/controller/PatientDatenController.java
+++ src/controller/PatientDatenController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.EPA;
+import model.Patient;
 import model.Untersuchungsbericht;
 
 import java.util.List;
@@ -9,15 +11,28 @@ public class PatientDatenController {
 	private EPAController ePAController;
 
 	public PatientDatenController(EPAController ePAController) {
+		this.ePAController = ePAController ;
 
 	}
 
-	public void PatientDatenUpdate(List<String> info) {
-
+	public void PatientDatenUpdate(String versicherungsnummer, String Vorname,String Nachname, String Address, String Geschlecht, String Birth, String pass) {
+		EPA epa = ePAController.getEPA();
+		Patient patient = epa.getPatient(versicherungsnummer);
+		patient.setVorname(Vorname);
+		patient.setNachname(Nachname);
+		patient.setAddress(Address);
+		patient.setGeschlecht(Geschlecht);
+		patient.setBirth(Birth);
+		patient.setPasswort(pass);
 	}
 
 	public List<Untersuchungsbericht> berichteAussuchen() {
+
+
+
 		return null;
 	}
+
+
 
 }
