c4ecdd675e9a1759bd79ff05711d435a3b27448a
nghhai1090 <76409868+nghhai1090@users.noreply.github.com>
2021-03-07 22:19:57 +0100

    fixed errors and views

--- src/view/DatenBearbeiten/DatenBearbeitenController.java
+++ src/view/DatenBearbeiten/DatenBearbeitenController.java
@@ -85,8 +85,22 @@ public class DatenBearbeitenController extends ScrollPane {
     @FXML
     void InfoAndern(ActionEvent event) throws IOException {
         Patient patient= epaController.getEPA().getPatient(epaController.getCurrLoggedIn());
-        if(pSetpass.getText().isEmpty()){HiddenText.setText("Geben Sie bitte ihres Passwort ein");HiddenText.setVisible(true);}
-        else if(!pSetpass.getText().equals(patient.getPasswort())){HiddenText.setVisible(true);}
+        if(pSetpass.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("");
+            alert.setHeaderText("Error");
+            String s ="Geben Sie bitte ihres Passwort ein";
+            alert.setContentText(s);
+            alert.show();
+        }
+        else if(!pSetpass.getText().equals(patient.getPasswort())){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("");
+            alert.setHeaderText("Error");
+            String s ="Sie haben das falsche Passwort eingegeben";
+            alert.setContentText(s);
+            alert.show();
+        }
         else{
             String s = pIsfemale.isSelected() ? "f" : "m";
             if(neuPass.getText().isEmpty()){
